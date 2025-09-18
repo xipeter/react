f1a00f5eac067f70a8a44a4017867c0e646c6377
Zongfei Xi <32719912+xipeter@users.noreply.github.com>
2025-09-17 22:20:59 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.tsx</code> and save to reload, I'm Peter
+					Edit <code>src/App.tsx</code> and save to reload, I'm Peter 2
 				</p>
 				<a
 					className="App-link"
